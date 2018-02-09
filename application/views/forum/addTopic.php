ae03e180410988228394bb4139b5657ecabcf663
dererumnatura <pollindrom@gmail.com>
2018-02-09 16:57:05 +0200

    phpstorm commit

---
+++ application/views/forum/addTopic.php
@@ -0,0 +1,4 @@
+<form action="/forum/add" method="post" class="add-message-form">
+    <input class="am-input" type="text" name="title">
+    <input class="submit" type="submit" value="Добавить">
+</form>
